ebedcd70dcf9634d61d76c24de592b1174ffe98b
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-07-21 06:41:29 -0500

    third commit

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-const lyricButton = document.getElementById('begin_lyric');
-const showLyricResult = document.getElementById('lyric-result')
+const lyricButton = document.getElementById('begin_lyric').addEventListener('click',event => {
+    if (event.target.className === 'text'){
+      
+}
+}) ;
 
-let lyricList = "";
 
-document.addEventListener('click', event => {
-    if(event.target.className ==='begin_lyric'){
-        function lyricList (list){
-            let num = list.length;
-            return Math.floor(Math.random()*num)
-            }
-    }
-})
+let array = [
+    location = ["Miami", "New York","California", "Texas", "Kansas"],
+    drinks = ["Tequila", "Soda","Milk", "Tea"],
+    celebrity =["Michael Jackson", "Obama", "Bennedict Cumberbatch", "elmo"],
+    action =["nod", "point", "kindly gesture", 'pop my collar']
 
-const location = [
-"Miami", "New York","California", "Texas", "Kansas"  
-]
-
-const drinks = [
-    "Tequila", "Soda","Milk", "Tea"
-]
-
-const celebrity =[
-    "Michael Jackson", "Obama", "Bennedict Cumberbatch", "elmo"
-]
-
-const action =[
-    "nod", "point", "kindly gesture", 'pop my collar'
-]
-
+];
 
 lyricButton.innerHTML = `We down in ${location} going hard tonight, 
 gonna throw down ${drinks} like its going out of style, 
@@ -35,7 +19,7 @@ Walk in the club fly like ${celebrity}.
 When the DJ sees me ${action} he clears the floor, 
 for famous dance action that I am known for. `;
 
-<h2 id='lyric-result'></h2>
+
 
 
 

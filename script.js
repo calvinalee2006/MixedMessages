297da04083c62ce1e878fd9689597506587990c6
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-07-22 05:48:11 -0500

    fourth commit

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const lyricButton = document.getElementById('begin_lyric').addEventListener('click',event => {
-    if (event.target.className === 'text'){
-      
-}
-}) ;
+const object = {
+    location: ["Miami", "New York", "California", "Texas", "Kansas"],
+    drinks: ["Tequila", "Soda", "Milk", "Tea"],
+    celebrity: ["Michael Jackson", "Obama", "Bennedict Cumberbatch", "elmo"],
+    action: ["nod", "point", "kindly gesture", 'pop my collar']
+};
 
+const getRandom = (array) => array[Math.floor(Math.random()*array.length)];
 
-let array = [
-    location = ["Miami", "New York","California", "Texas", "Kansas"],
-    drinks = ["Tequila", "Soda","Milk", "Tea"],
-    celebrity =["Michael Jackson", "Obama", "Bennedict Cumberbatch", "elmo"],
-    action =["nod", "point", "kindly gesture", 'pop my collar']
+document.getElementById('begin_lyric').addEventListener('click', event => { 
+document.getElementById('text').innerHTML = `We down in ${getRandom(object.location)}
+ going hard tonight, 
+gonna throw down ${getRandom(object.drinks)} like its going out of style, 
+Walk in the club fly like ${getRandom(object.celebrity)}. 
+When the DJ sees me ${getRandom(object.action)} he clears the floor, 
+for famous dance action that I am known for. `; }
+);
 
-];
-
-lyricButton.innerHTML = `We down in ${location} going hard tonight, 
-gonna throw down ${drinks} like its going out of style, 
-Walk in the club fly like ${celebrity}. 
-When the DJ sees me ${action} he clears the floor, 
-for famous dance action that I am known for. `;
 
 
 
